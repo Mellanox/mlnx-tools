Mellanox/mlnx-tools

commit d7adfdf549f2545b5bb5c03a75222a217e45902a

udev scripts: Support network interfaces rename on DPU only
Avoid renaming network interfaces unless operating on a BlueField DPU.

Signed-off-by: Vladimir Sokolovsky <vlad@nvidia.com>

udev/scripts/vf-net-link-name.sh
@@ -7,7 +7,12 @@ PORT_NAME=`echo ${1} | sed -e "s/c[[:digit:]]\+//"`
 IFINDEX=$3
 
 # need the PATH for BF ARM lspci to work
-PATH=/bin:/sbin:/usr/bin:/usr/sbin
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin
+
+is_bf=`lspci -s 00:00.0 2> /dev/null | grep -wq "PCI bridge: Mellanox Technologies" && echo 1 || echo 0`
+if [ $is_bf -ne 1 ]; then
+	exit 0
+fi
 
 if [[ "$ID_NET_DRIVER" != *"mlx5"* ]]; then
     exit 1
@@ -46,7 +51,6 @@ function get_mh_bf_rep_name() {
         done
 }
 
-is_bf=`lspci -s 00:00.0 2> /dev/null | grep -wq "PCI bridge: Mellanox Technologies" && echo 1 || echo 0`
 if [ $is_bf -eq 1 ]; then
         num_of_pf=`lspci 2> /dev/null | grep -w "network" | wc -l`
         if [ $num_of_pf -gt 2 ]; then

udev/scripts/auxdev-sf-netdev-rename
@@ -15,6 +15,13 @@
 SFNUM=$1
 IFINDEX=$2
 
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin
+
+is_bf=`lspci -s 00:00.0 2> /dev/null | grep -wq "PCI bridge: Mellanox Technologies" && echo 1 || echo 0`
+if [ $is_bf -ne 1 ]; then
+	exit 0
+fi
+
 for sf_ndev in `ls /sys/class/net/`; do
 	_ifindex=`cat /sys/class/net/$sf_ndev/ifindex | head -1 2>/dev/null`
 	if [ "$_ifindex" = "$IFINDEX" ]

udev/scripts/sf-rep-netdev-rename
@@ -3,6 +3,13 @@
 PORT_NAME=$1
 IFINDEX=$2
 
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin
+
+is_bf=`lspci -s 00:00.0 2> /dev/null | grep -wq "PCI bridge: Mellanox Technologies" && echo 1 || echo 0`
+if [ $is_bf -ne 1 ]; then
+	exit 0
+fi
+
 for rep_ndev in `ls /sys/class/net/`; do
 	_ifindex=`cat /sys/class/net/$rep_ndev/ifindex | head -1 2>/dev/null`
 	if [ "$_ifindex" = "$IFINDEX" ]

commit e66875a78071a7d760c5c29ecfa41bf59484e1ba

udev scripts: Prevent 82-net-setup-link.rules from setting invalid NAME
The 82-net-setup-link.rules UDEV rule calls vf-net-link-name.sh and then
sets NAME="$env{NAME}". But in some cases, vf-net-link-name.sh does not
return "NAME" and the result is error messages such as:

"systemd-udevd[164885]: Error changing net interface name 'eth101' to '': Invalid argument"

Additionally, this can break other UDEV rules that run after
82-net-setup-link.rules. vf-net-link-name.sh should always return
a valid interface name.

Fixes: d7adfdf549f2 ("udev scripts: Support network interfaces rename on DPU only")
Signed-off-by: Mark Haywood <mark.haywood@oracle.com>

udev/rules.d/82-net-setup-link.rules
@@ -1,12 +1,12 @@
 SUBSYSTEM=="net", NAME=="side-*-eth[0-9]*", GOTO="net_setup_skip_link_name"
 
 SUBSYSTEM=="net", ACTION=="add", ATTR{phys_switch_id}!="", ATTR{phys_port_name}!="", ATTR{phys_port_name}!="*pf*sf*" \
-        IMPORT{program}="/lib/udev/vf-net-link-name.sh $attr{phys_port_name} $attr{phys_switch_id} $attr{ifindex}" \
+        IMPORT{program}="/lib/udev/vf-net-link-name.sh $name $attr{phys_port_name} $attr{phys_switch_id} $attr{ifindex}" \
         NAME="$env{NAME}", RUN+="/lib/udev/mlnx_bf_udev $env{NAME}", GOTO="net_setup_skip_link_name"
 
 
 SUBSYSTEM=="net", ACTION=="add", ATTR{phys_port_name}!="", ATTR{phys_port_name}!="*pf*sf*" \
-        IMPORT{program}="/lib/udev/vf-net-link-name.sh $attr{phys_port_name}" \
+        IMPORT{program}="/lib/udev/vf-net-link-name.sh $name $attr{phys_port_name}" \
         NAME="$env{NAME}", RUN+="/lib/udev/mlnx_bf_udev $env{NAME}"
 
 LABEL="net_setup_skip_link_name"

udev/scripts/vf-net-link-name.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-SWID=$2
+ORIG_NAME=$1
+SWID=$3
 # might be pf0vf1 so only get vf number
-PORT=${1##*f}
-PORT_NAME=`echo ${1} | sed -e "s/c[[:digit:]]\+//"`
-IFINDEX=$3
+PORT=${2##*f}
+PORT_NAME=`echo ${2} | sed -e "s/c[[:digit:]]\+//"`
+IFINDEX=$4
 
 # need the PATH for BF ARM lspci to work
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin
 
 is_bf=`lspci -s 00:00.0 2> /dev/null | grep -wq "PCI bridge: Mellanox Technologies" && echo 1 || echo 0`
 if [ $is_bf -ne 1 ]; then
+        echo "NAME=$ORIG_NAME"
 	exit 0
 fi
 
 if [[ "$ID_NET_DRIVER" != *"mlx5"* ]]; then
-    exit 1
+        echo "NAME=$ORIG_NAME"
+        exit 1
 fi
 
 function get_mh_bf_rep_name() {